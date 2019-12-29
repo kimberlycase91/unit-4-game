d5fa039e0ee969dc4600dd69b0b85ed0e0d5a38f
kimberlycase91 <kimberlycase91@gmail.com>
2019-12-29 10:40:58 -0700

    finished working game code-cleaned up code by adding random number generator template function-added background image, container background, border, and top margin.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,38 +1,64 @@
 //global variables
 var targetNumber; //holds random/target number for each game
-var pinkCrystal; //holds random value for pink crystal button
-var orangeCrystal; //holds random value for orange crystal button
-var blueCrystal; //holds random value for blue crystal button
-var purpleCrystal; //holds random value for purple crystal button
+var crystalButton;
 var wins = 0; //holds win count
 var losses = 0; //holds loss count
 var currentTotal = 0 //holds current total count to be added to during gameplay
 
-// display random number between 19 and 120 at start of game
-$(document).ready(function(){
-    targetNumber = Math.floor(Math.random() * (120-19) + 19);
 
-    $("#target-number-div").html(targetNumber);
-    $("#total-score-div").html(currentTotal);
+$(document).ready(function () {
 
-// assign random number to each crystalbutton (between 1-12)
-    pinkCrystal = Math.floor(Math.random() * (12-1) + 1);
-    $("#pink-crystal").attr(pinkCrystal);
-    orangeCrystal = Math.floor(Math.random() * (12-1) + 1);
-    blueCrystal = Math.floor(Math.random() * (12-1) + 1);
-    purpleCrystal = Math.floor(Math.random() * (12-1) + 1);
+    Start();
 
-//set currentTotal to zero
-    currentTotal = 0;
-
-//when you click on a crystal add the value to the currentTotal 
-    $(".crystal-button").on("click", function(){
-    
-    $("#target-number-div").text(currentTotal + this.val());
-   
-//if currentTotal equals randomNumber, add one to wins and generate new randomNumber and crystal button numbers and clear currentTotal
-
-//if currentTotal > randomNumber, add one to losses and generate new randomNumber and crystal button numbers and clear currentTotal
- })
+    //when you click on a crystal add the value to the currentTotal 
+    $(".crystal-button").on("click", handleClick)
 
 });
+
+function handleClick() {
+    var value = $(this).val();
+
+    currentTotal += parseInt(value);
+
+    $("#total-score-div").html(currentTotal);
+    //if currentTotal equals randomNumber, add one to wins and generate new randomNumber and crystal button numbers and clear currentTotal
+    if (currentTotal === targetNumber) {
+        wins++;
+        $("#wins-div").html(wins);
+        Start();
+    }
+    //if currentTotal > randomNumber, add one to losses and generate new randomNumber and crystal button numbers and clear currentTotal
+    if (currentTotal > targetNumber) {
+        losses++;
+        $("#losses-div").html(losses);
+        Start();
+    }
+}
+
+function getRandomInt(low, high) {
+    return Math.floor(Math.random() * (high - low) + low)
+}
+
+function Start() {
+    // display random number between 19 and 120 at start of game
+    targetNumber = getRandomInt(19, 120);
+    $("#target-number-div").html(targetNumber);
+
+    // assign random number to each crystalbutton (between 1-12)
+    // pinkCrystal = getRandomInt(1, 12);
+    // $("#pink-crystal").val(pinkCrystal);
+    // orangeCrystal = getRandomInt(1, 12);
+    // $("#orange-crystal").val(orangeCrystal);
+    // blueCrystal = getRandomInt(1, 12);
+    // $("#blue-crystal").val(blueCrystal);
+    // purpleCrystal = getRandomInt(1, 12);
+    // $("#purple-crystal").val(purpleCrystal);
+
+    $(".crystal-button").each(function (index, item) {
+        $(item).val(getRandomInt(1, 12))
+    })
+
+    //set currentTotal to zero and display on page
+    currentTotal = 0;
+    $("#total-score-div").html(currentTotal);
+}
